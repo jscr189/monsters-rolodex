8af18406480dd94d3e0cf1d859b325aad9937264
jscr189 <jscr189@gmail.com>
2020-05-01 22:21:12 +0530

    Updates

---
+++ precache-manifest.6d992d1efff473ea24786aed020be531.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a146847eaa952ee2da50cafc1232ac60",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "59b6997d916af22df33f",
+    "url": "/monsters-rolodex/static/css/main.3cfe5839.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b6997d916af22df33f",
+    "url": "/monsters-rolodex/static/js/main.387f1853.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
