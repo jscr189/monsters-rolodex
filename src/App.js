d990fc26a2a665e55d3806ccf52c70c9a6a7b97e
jscr189 <jscr189@gmail.com>
2020-05-01 19:57:14 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './Components/Card-List/card-list.component';
+import { SearchBox } from './Components/SearchBox/search-box.component'
 
-function App() {
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      monsters : [],
+      searchField : ''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then(
+      users => users.json()
+    ).then(
+      json => this.setState({monsters : json})
+    )
+  }
+
+  handleChange = e => {
+    this.setState({searchField : e.target.value},() => console.log(this.state));
+  }
+
+  render(){
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(
+      monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+          <h1>Monsters Rolodex</h1>
+          <SearchBox placeholder='Search Monster' 
+          handleChange={e => {
+            this.setState({searchField : e.target.value},() => console.log(this.state))}} />
+          
+          <CardList monsters={filteredMonsters}/>
+      </div>
+    );    
+  }
+}
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +61,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
